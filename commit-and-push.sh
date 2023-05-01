f15f8c6911de38eb459079a0331951d549883e77
vincexl <vincexlcooper@gmail.com>
2023-05-01 11:14:42 -0700

    allow for custom commit message input

--- commit-and-push.sh
+++ commit-and-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+message=${1}
+
 # Copy the latest version of the .xml file to the Git repository directory
 cp /Users/vhu/dev/lh-version-control/tecan-backup/rnd-t7-evo.robo.counsyl.com/CustomLCS.xml /Users/vhu/dev/lh-version-control/rnd-t7-evo/CustomLCS.xml
 
@@ -11,7 +13,7 @@ else
   git add CustomLCS.xml
 
   # Commit the changes with a message indicating that the .xml file has been updated
-  git commit -m "Update .xml file"
+  git commit -m "$message"
 
   # Push the changes to the remote repository
   git push origin master
