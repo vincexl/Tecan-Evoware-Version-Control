7315769d0ee756ca25eeed554faf58a9bce69f83
vincexl <vincexlcooper@gmail.com>
2023-05-01 10:04:59 -0700

    revise commit and push shell

--- commit-and-push.sh
+++ commit-and-push.sh
@@ -3,6 +3,11 @@
 # Copy the latest version of the .xml file to the Git repository directory
 cp /Users/vhu/dev/lh-version-control/tecan-backup/rnd-t7-evo.robo.counsyl.com/CustomLCS.xml /Users/vhu/dev/lh-version-control/rnd-t7-evo/CustomLCS.xml
 
+# Exit if there are no changes to commit
+if [ -z "$(git status --porcelain)" ]; then
+  exit 0
+fi
+
 # Add the file to the Git repository
 git add CustomLCS.xml
 
